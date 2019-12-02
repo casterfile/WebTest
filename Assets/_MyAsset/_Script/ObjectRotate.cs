a4eac6b39d41a6cbbd39f7d5e1b2f2686aa56108
tonyMac <tonyMac@tonys-MacBook-Pro.local>
2019-12-02 10:51:10 +0800

    Add all my files

---
+++ Assets/_MyAsset/_Script/ObjectRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectRotate : MonoBehaviour {
+
+    private float RotationSpeed = 90.0f;
+    private string DataString = "";
+    public Text DataText;
+    // Use this for initialization
+    void Start () {
+        DataString = SystemInfo.deviceUniqueIdentifier;
+        DataText.text = DataString;
+        print("DataString: "+ DataString);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * (RotationSpeed * Time.deltaTime));
+
+    }
+}
